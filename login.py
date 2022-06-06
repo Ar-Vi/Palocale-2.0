e8e5997b553a77b7f813e7d2d96b4480243d7273
Arya Vishnu <aryavishnu06@gmail.com>
2022-06-06 15:30:16 -0400

    Initialized stuff

---
+++ login.py
@@ -0,0 +1,14 @@
+import pygame
+import os
+import const.py
+
+WIN = pygame.display.set_mode(1080, 1920)
+pygame.display.set_caption("Palocale")
+
+WHITE = (255, 255, 255)
+FPS = 60
+
+WIN.fill(WHITE)
+
+
+
