acd0928d178b1c0a1b4571816747143f35bbcf4a
amberosia <706648@pdsb.net>
2022-06-07 08:36:49 -0400

    a
    game loop and win colour

--- login.py
+++ login.py
@@ -1,14 +1,26 @@
 import pygame
 import os
-import const.py
+import const
+WHITE = (255, 255, 255)
+ORANGE = (248, 173, 24)
 
-WIN = pygame.display.set_mode(1080, 1920)
+(wid, ht) = (540, 960)
+WIN = pygame.display.set_mode((wid, ht))
 pygame.display.set_caption("Palocale")
 
-WHITE = (255, 255, 255)
 FPS = 60
-
+font = pygame.font.SysFont('calibri.ttf', 24)
+img = font.render('alocale', True, ORANGE)
+WIN.blit(img, (270, 300))
 WIN.fill(WHITE)
+pygame.display.flip()
 
+run = True
+clock = pygame.time.Clock()
+while run:
+    dt = clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
 
