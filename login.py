e3a9c8e8ef6e3d63107ff3396dd594b5a6a37141
Arya Vishnu <aryavishnu06@gmail.com>
2022-06-08 15:54:27 -0400

    Login Logic

--- login.py
+++ login.py
@@ -1,26 +1,72 @@
 import pygame
 import os
 import const
-WHITE = (255, 255, 255)
-ORANGE = (248, 173, 24)
 
-(wid, ht) = (540, 960)
-WIN = pygame.display.set_mode((wid, ht))
+WIDTH, HEIGHT = 405, 720
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Palocale")
 
+WHITE = (255, 255, 255)
 FPS = 60
-font = pygame.font.SysFont('calibri.ttf', 24)
-img = font.render('alocale', True, ORANGE)
-WIN.blit(img, (270, 300))
-WIN.fill(WHITE)
-pygame.display.flip()
 
-run = True
-clock = pygame.time.Clock()
-while run:
-    dt = clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+load_images = []
+for num in range(1, 8):
+    load_images.append(pygame.image.load(os.path.join("assets", str(num) + ".png")))
+
+welcome_p, welcome_button, interests_p, ints_button, ints_select, verify_p, done_p = \
+    load_images
+
+"""
+ ------ COORDINATES ------ 
+Name field: 73, 360 to 331, 394
+Age field: 161, 412 to 244, 448
+Welc button: 163, 490 to 241, 569
+
+Ints button: 163, 556 to 241, 635
+
+Veri button: 163, 508 to 241, 587
+
+Done button: 163, 491 to 241, 569
+"""
+
+def page(currentPage, listOfAss):
+    cursor_x, cursor_y = pygame.mouse.get_pos()
+    
+
+    if pygame.mouse.get_pressed(3)[0]:
+        listOfAss = [[interests_p, (0,0)]]
+        if 32 < cursor_x < 60:
+            pass
+        elif 107 < cursor_x < 140:
+          pass
+            
+    else:
+        pass
+
+    return listOfAss
+
+
+
+def main():
+    assToDisplay = [[welcome_p, (0,0)]]
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        dt = clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        assToDisplay = page(assToDisplay[0], assToDisplay)
+        WIN.fill(WHITE)
+        for ass in assToDisplay:
+            WIN.blit(ass[0], ass[1])
+
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
 
 
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import pygame
-fonts = pygame.font.get_fonts()
-print(len(fonts))
-for f in fonts:
-    print(f)
+"""Name field: 73, 360 to 331, 394
+Age field: 161, 412 to 244, 448
+Welc button: 163, 490 to 241, 569
+
+Ints button: 163, 556 to 241, 635
+
+Veri button: 163, 508 to 241, 587
+
+Done button: 163, 491 to 241, 569"""
+print("hi")
