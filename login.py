8ef056053ae3d5b6766e557ea26c513accdc5ed9
Arya Vishnu <aryavishnu06@gmail.com>
2022-06-08 21:41:41 -0400

    Finished welcome page

--- login.py
+++ login.py
@@ -19,13 +19,15 @@ welcome_p, welcome_button, interests_p, ints_button, ints_select, verify_p, done
     load_images
 
 
-def page(currentPage, listOfAss):
+def page(currentPage, listOfAss, input_boxes):
     cursor_x, cursor_y = pygame.mouse.get_pos()
     
-
-    if pygame.mouse.get_pressed(3)[0]:
+    if currentPage == welcome_p and len(input_boxes[0].text) > 1 and len(input_boxes[0].text) > 1:
+        listOfAss.append([welcome_button, (0,0)])
+    if pygame.mouse.get_pressed(3)[0]: # 
         if currentPage == welcome_p and 163 < cursor_x < 241 and 490 < cursor_y < 569:
             listOfAss = [[interests_p, (0,0)]]
+            input_boxes = []
 
         elif 107 < cursor_x < 140:
           pass
@@ -33,12 +35,12 @@ def page(currentPage, listOfAss):
     else:
         pass
 
-    return listOfAss
+    return listOfAss, input_boxes
 
 
 """
  ------ COORDINATES ------ 
-Name field: 73, 360 to 331, 394
+Name field: 73, 358 to 331, 394
 Age field: 161, 412 to 244, 448
 Welc button: 163, 490 to 241, 569
 
@@ -51,8 +53,8 @@ Done button: 163, 491 to 241, 569
 
 def main():
     assToDisplay = [[welcome_p, (0,0)]]
-    input_box1 = InputBox(73, 360, 258, 34)
-    input_box2 = InputBox(161, 412, 83, 36)
+    input_box1 = InputBox(73, 358, 258, 38)
+    input_box2 = InputBox(161, 412, 83, 38)
     input_boxes = [input_box1, input_box2]#
     clock = pygame.time.Clock()
     run = True
@@ -65,7 +67,7 @@ def main():
             for box in input_boxes:
                 box.handle_event(event)
         
-        assToDisplay = page(assToDisplay[0][0], assToDisplay)
+        assToDisplay, input_boxes = page(assToDisplay[0][0], assToDisplay, input_boxes)
         WIN.fill(WHITE)
         for ass in assToDisplay:
             WIN.blit(ass[0], ass[1])
@@ -75,7 +77,7 @@ def main():
             box.draw(WIN)
         
         #pygame.draw.rect(WIN, ORANGE, (73, 360, 258, 34), 2)
-       
+        
         pygame.display.update()
 
     pygame.quit()
